2dd381d5425419dc585dccdfc310d70dc1bacd76
freehoon <freehoon009@gmail.com>
2020-06-23 07:30:26 +0900

    복잡한 UI 완료

--- flutter_app007_ui_sample/lib/page1.dart
+++ flutter_app007_ui_sample/lib/page1.dart
@@ -1,13 +1,205 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class Page1 extends StatelessWidget {
+
+  final dummyItems = [
+    'https://cdn.pixabay.com/photo/2020/02/21/12/58/toddler-hand-4867454_1280.jpg',
+    'https://cdn.pixabay.com/photo/2020/06/18/15/28/chair-5313970__480.jpg',
+    'https://cdn.pixabay.com/photo/2020/05/30/23/12/pigs-5240702__480.jpg',
+    'https://cdn.pixabay.com/photo/2020/06/02/19/20/evening-sun-5252271__480.jpg',
+    'https://cdn.pixabay.com/photo/2020/06/11/02/13/fantasy-5284897__480.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home 페이지',
-        style: TextStyle(fontSize: 40),
+    return ListView(
+      children: <Widget>[
+        _buildTop(),
+        _buildMiddle(),
+        _buildBottom(),
+      ],
+    );
+  }
+
+  Widget _buildTop() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  print('1줄 택시 클릭');
+                },
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('택시'),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('1줄 블랙 클릭');
+                },
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('블랙'),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('1줄 타이크 클릭');
+                },
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('바이크'),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('1줄 대리 클릭');
+                },
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('대리'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  print('2줄 택시 클릭');
+                },
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('택시'),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('2줄 블랙 클릭');
+                },
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('블랙'),
+                  ],
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  print('2줄 바이크 클릭');
+                },
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('바이크'),
+                  ],
+                ),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: GestureDetector(
+                  onTap: () {
+                    print('2줄 대리 클');
+                  },
+                  child: Column(
+                    children: <Widget>[
+                      Icon(
+                        Icons.local_taxi,
+                        size: 40,
+                      ),
+                      Text('대리'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _buildMiddle() {
+    return CarouselSlider(
+      options: CarouselOptions(height: 150.0),
+      items: dummyItems.map((url) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                //decoration: BoxDecoration(
+                //    color: Colors.amber
+                //),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      url,
+                      fit: BoxFit.cover,
+                    )
+                )
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+
+  Widget _buildBottom() {
+    final items = List.generate(10, (i) {
+      return ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[이벤트] 이것은 공지사항 입니다'),
+      );
+    });
+    return ListView(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      children: items,
     );
   }
 }
